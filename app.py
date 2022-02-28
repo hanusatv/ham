db527604cdd022861ca46699913b732285d2f718
hanusatv <hanusatv@gmail.com>
2022-02-28 16:31:57 +0100

    fixing the white letters

--- app.py
+++ app.py
@@ -34,20 +34,26 @@ def evaluateGuess(answer, guess, tryNo):
     logString = ""
     icons = f'{tryNo}'
     letterList = ["", "", "", "", ""]
+    # Kanna um svarið er rætt
     if guess == answer:
-        icons += f'\x1b[0;30;42m {guess.upper()} \x1b[0m \nTillukku!! Tú vann \U0001F64A \U0001F44C \U0001F973'
+        icons += f'\x1b[0;30;42m {guess[0].upper()}  {guess[1].upper()}  {guess[2].upper()}  {guess[3].upper()}  {guess[4].upper()} \x1b[0m\nTillukku!! Tú vann \U0001F64A \U0001F44C \U0001F973'
         return icons
+    # Finn øll tey grønu svarini
     for i, c in enumerate(guess):
         if c == answer[i]:
-            icons += f'\x1b[0;30;42m {c.upper()} \x1b[0m'
+            letterList[i] = f'\x1b[0;30;42m {c.upper()} \x1b[0m'
             logString += c
-        elif c in answer and answer.count(c) > logString.count(c):
-            icons += f'\x1b[0;30;43m {c.upper()} \x1b[0m'
+    #Finn øll tey gulu svarini
+    for i, c in enumerate(guess):
+        if letterList[i] == "" and logString.count(c) < answer.count(c):
+            letterList[i] = f'\x1b[0;30;43m {c.upper()} \x1b[0m'
             logString += c
-        else:
-            icons += f'\x1b[6;30;47m {c.upper()} \x1b[0m'
-    icons += "\n"
+    #Set restina til hvítt
+    for i, c in enumerate(guess):
+        if letterList[i] == "":
+            letterList[i] = f'\x1b[6;30;47m {c.upper()} \x1b[0m'
 
+    icons = f'{tryNo}{letterList[0]}{letterList[1]}{letterList[2]}{letterList[3]}{letterList[4]}\n'
     return (icons)
 
 
@@ -65,4 +71,5 @@ if __name__ == "__main__":
                 break
             tryNo += 1
         if tryNo == 7:
-            print(f'Øvv. Tú kláraði tað ikki \U0001F622 \nOrðið var {answer}')
+            print(
+                f'Øvv. Tú kláraði tað ikki \U0001F622 \nOrðið var "{answer}"')
